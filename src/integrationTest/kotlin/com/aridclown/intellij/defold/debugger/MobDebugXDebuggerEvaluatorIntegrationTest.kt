da20b55ab7a9ca971a632a66ca6aeafb7be24144
Arid Clown <764655+aridclown@users.noreply.github.com>
2025-10-28 11:22:05 +0900

    add integration tests for MobDebugXDebuggerEvaluator

--- src/integrationTest/kotlin/com/aridclown/intellij/defold/debugger/MobDebugXDebuggerEvaluatorIntegrationTest.kt
+++ src/integrationTest/kotlin/com/aridclown/intellij/defold/debugger/MobDebugXDebuggerEvaluatorIntegrationTest.kt
@@ -3,86 +3,150 @@ package com.aridclown.intellij.defold.debugger
 import com.aridclown.intellij.defold.debugger.eval.MobDebugEvaluator
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.xdebugger.impl.XSourcePositionImpl
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 
+/**
+ * Integration tests for MobDebugXDebuggerEvaluator expression range selection.
+ */
 class MobDebugXDebuggerEvaluatorIntegrationTest : BasePlatformTestCase() {
 
-    fun `test skips callee identifier`() {
-        val file = myFixture.configureByText(
-            "script.lua",
-            """
-            function foo()
-                hash("foo")
-            end
-            """.trimIndent()
-        )
-        val document = myFixture.editor.document
-        val frameLine = document.getLineNumber(document.text.indexOf("hash"))
-        val evaluator = MobDebugXDebuggerEvaluator(
+    private fun createEvaluator(virtualFile: VirtualFile, frameLine: Int): MobDebugXDebuggerEvaluator {
+        return MobDebugXDebuggerEvaluator(
             project,
             MobDebugEvaluator(mockk<MobDebugProtocol>(relaxed = true)),
             frameIndex = 0,
-            framePosition = XSourcePositionImpl.create(file.virtualFile, frameLine)
+            framePosition = XSourcePositionImpl.create(virtualFile, frameLine)
         )
+    }
 
-        val offset = document.text.indexOf("hash")
+    private fun assertExpressionRangeIs(code: String, searchText: String, expected: String?) {
+        val file = myFixture.configureByText("script.lua", code)
+        val document = myFixture.editor.document
+        val offset = document.text.indexOf(searchText)
+        val frameLine = document.getLineNumber(offset)
+        val evaluator = createEvaluator(file.virtualFile, frameLine)
+
         val range = evaluator.getExpressionRangeAtOffset(project, document, offset, false)
 
-        assertThat(range).isNull()
+        if (expected == null) {
+            assertThat(range).isNull()
+        } else {
+            assertThat(range?.substring(document)).isEqualTo(expected)
+        }
     }
 
-    fun `test evaluates call receiver`() {
-        val file = myFixture.configureByText(
-            "script.lua",
-            """
-            function foo()
-                local go = msg.url()
-                go.get_position()
-            end
-            """.trimIndent()
-        )
-        val document = myFixture.editor.document
-        val callOffset = document.text.indexOf("go.get_position")
-        val frameLine = document.getLineNumber(callOffset)
-        val evaluator = MobDebugXDebuggerEvaluator(
-            project,
-            MobDebugEvaluator(mockk<MobDebugProtocol>(relaxed = true)),
-            frameIndex = 0,
-            framePosition = XSourcePositionImpl.create(file.virtualFile, frameLine)
-        )
+    // Tests that should return null
 
-        val range = evaluator.getExpressionRangeAtOffset(project, document, callOffset, false)
+    fun `test skips callee identifier`() = assertExpressionRangeIs(
+        """
+        function foo()
+            hash("foo")
+        end
+        """.trimIndent(),
+        "hash",
+        null
+    )
 
-        assertThat(range?.substring(document)).isEqualTo("go")
-    }
+    fun `test skips method name`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local go = msg.url()
+            go.get_position()
+        end
+        """.trimIndent(),
+        "get_position",
+        null
+    )
 
-    fun `test skips method name`() {
-        val file = myFixture.configureByText(
-            "script.lua",
-            """
-            function foo()
-                local go = msg.url()
-                go.get_position()
-            end
-            """.trimIndent()
-        )
-        val document = myFixture.editor.document
-        val methodOffset = document.text.indexOf("get_position")
-        val frameLine = document.getLineNumber(document.text.indexOf("go.get_position"))
-        val evaluator = MobDebugXDebuggerEvaluator(
-            project,
-            MobDebugEvaluator(mockk<MobDebugProtocol>(relaxed = true)),
-            frameIndex = 0,
-            framePosition = XSourcePositionImpl.create(file.virtualFile, frameLine)
-        )
+    fun `test skips function name in call`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local result = math.sqrt(16)
+        end
+        """.trimIndent(),
+        "sqrt",
+        null
+    )
 
-        val range = evaluator.getExpressionRangeAtOffset(project, document, methodOffset, false)
+    // Tests that should return expressions
 
-        assertThat(range).isNull()
-    }
+    fun `test evaluates call receiver`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local go = msg.url()
+            go.get_position()
+        end
+        """.trimIndent(),
+        "go.get_position",
+        "go"
+    )
+
+    fun `test evaluates variable identifier`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local value = 42
+            print(value)
+        end
+        """.trimIndent(),
+        "value)",
+        "value"
+    )
+
+    fun `test evaluates table field access`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local t = {x = 10}
+            print(t.x)
+        end
+        """.trimIndent(),
+        "t.x",
+        "t"
+    )
+
+    fun `test evaluates self reference in method`() = assertExpressionRangeIs(
+        """
+        function Obj:method()
+            self.value = 10
+        end
+        """.trimIndent(),
+        "self",
+        "self"
+    )
+
+    fun `test evaluates global variable`() = assertExpressionRangeIs(
+        """
+        function foo()
+            print(_G.some_global)
+        end
+        """.trimIndent(),
+        "_G",
+        "_G"
+    )
+
+    fun `test evaluates Defold API object`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local v = vmath.vector3()
+        end
+        """.trimIndent(),
+        "vmath",
+        "vmath"
+    )
+
+    fun `test evaluates array index base`() = assertExpressionRangeIs(
+        """
+        function foo()
+            local arr = {1, 2, 3}
+            print(arr[1])
+        end
+        """.trimIndent(),
+        "arr[",
+        "arr"
+    )
 
     private fun TextRange.substring(document: Document): String =
         document.getText(this)
